44bd05193fdba835d8df97c4ed6fb5955e2a74d3
Prohnov Pavel <54280007+pr0xnov@users.noreply.github.com>
2021-09-21 13:02:04 +0300

    fixed countDownTimer

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-// Время для обратного отсчёта
-let CountdownTimer = {
-  selector: "#timer-1",
-  targetDate: new Date("oct 10 2021 17:07:50"),
-};
+import refs from './refs.js'
+const { days, hours, mins, secs, smallHeader } = refs
 
-
-// Обновляем таймер
-const countDownFunc = setInterval(function () {
-  // Время на данный момент
-  let date = new Date().getTime();
-
-  //   Промежуток времени от установленого
-  time = CountdownTimer.targetDate - date;
-
-  /*
-   * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-   * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-   */
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-  /*
-   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-   * остатка % и делим его на количество миллисекунд в одном часе
-   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-   */
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-  /*
-   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-   */
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-  /*
-   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-   * миллисекунд в одной секунде (1000)
-   */
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-  document.querySelector('[data-value="days"]').innerText = days;
-  document.querySelector('[data-value="hours"]').innerText = hours;
-  document.querySelector('[data-value="mins"]').innerText = mins;
-  document.querySelector('[data-value="secs"]').innerText = secs;
-  if (time < 0) {
-    clearInterval(countDownFunc);
+class CountdownTimer {
+  constructor(selector, targetDate) {
+    this.selector = selector
+    this.targetDate = targetDate.getTime()
+    this.intervalId = null
+    this.time = 0
   }
-}, 1000);
-console.log(countDownFunc);
+  
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      let currentDate = Date.now()
+
+      this.time = this.targetDate - currentDate
+
+      this.insertData(days, this.getDays(this.time))
+      this.insertData(hours, this.getHours(this.time))
+      this.insertData(mins, this.getMins(this.time))
+      this.insertData(secs, this.getSecs(this.time))
+    }, 1000)
+  }
+
+  padValue(value, num, symbol) {
+    return String(value).padStart(num, symbol)
+  }
+
+  getDays(time) {
+    return this.padValue(Math.floor(time / (1000 * 60 * 60 * 24)), 3, '0')
+  }
+  getHours(time) {
+    return this.padValue(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), 2, '0')
+  }
+  getMins(time) {
+    return this.padValue(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)), 2, '0')
+  }
+  getSecs(time) {
+    return this.padValue(Math.floor((time % (1000 * 60)) / 1000))
+  }
+
+  insertData(place, value) {
+    place.textContent = value
+  }
+}
+
+const newTimer = new CountdownTimer('#timer-1', new Date('Dec 31, 2021'))
+newTimer.startTimer()
+
